3b11b3ad7618436339174d56cae0fbdd857adbbf
Marc-Antoine Lacasse <m.a.l.musique@gmail.com>
2026-04-08 15:56:43 -0400

    feat(playground): restore visual editor demo with new architecture

--- playground/main.ts
+++ playground/main.ts
@@ -1,1 +1,32 @@
 import './style.css';
+import {DefaultEditorDomResolver} from "../src/dom";
+import {DefaultMarkdownProcessor, ModuloEditor} from "../src";
+import {TextareaInputAdapter} from "../src/input";
+import {HiddenTextareaBridge} from "../src/textarea";
+import {createDefaultToolbarPlugins} from "../src/plugins";
+import {HtmlPreviewAdapter} from "../src/output";
+import {
+    DEFAULT_HTML_SANITIZER_CONFIG,
+    DomPurifyHtmlSanitizer,
+    PlainTextMarkdownParser
+} from "../src/markdown";
+
+const root = document.querySelector('[data-mo-editor]') as HTMLElement;
+
+const domResolver = new DefaultEditorDomResolver();
+const slots = domResolver.resolve(root);
+
+const editor = new ModuloEditor({
+    root,
+    domResolver,
+    input: new TextareaInputAdapter(),
+    output: new HtmlPreviewAdapter(),
+    markdown: new DefaultMarkdownProcessor(
+        new PlainTextMarkdownParser(), // TODO add marked Processor
+        new DomPurifyHtmlSanitizer(DEFAULT_HTML_SANITIZER_CONFIG)
+    ),
+    textareaBridge: new HiddenTextareaBridge(),
+    plugins: createDefaultToolbarPlugins(slots.toolbar!)
+});
+
+editor.init();
